2aa6759eda60b16baa82f6dda6b286fecfca4b4e
ellenfel <ellfelfen@gmail.com>
2024-09-07 23:33:52 +0300

    Configured PostgreSQL database

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -121,3 +121,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_manager_db',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
