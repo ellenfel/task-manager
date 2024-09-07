76b144a1c75737a99996736b081d72301a886af9
ellenfel <ellfelfen@gmail.com>
2024-09-07 23:33:54 +0300

    Defined models for users and tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
