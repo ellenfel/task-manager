8aca819a29834cf902a3a0d7a48a2f1173e5e0b0
ellenfel <ellfelfen@gmail.com>
2024-09-10 10:00:48 +0300

    Created views for users and tasks

--- tasks/views.py
+++ tasks/views.py
@@ -3,5 +3,15 @@ from .models import Task
 from .serializers import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
+    """
+    A view set for managing tasks.
+
+    This view set provides CRUD (Create, Retrieve, Update, Delete) operations for tasks.
+
+    Attributes:
+        queryset (QuerySet): The queryset of all tasks.
+        serializer_class (Serializer): The serializer class used for task serialization.
+
+    """
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
-from rest_framework import viewsets
-from .models import User
-from .serializers import UserSerializer
+# Importing the necessary modules and classes
+from rest_framework import viewsets  # Provides a set of views for handling CRUD operations
+from .models import User  # Importing the User model from the current directory
+from .serializers import UserSerializer  # Importing the UserSerializer from the current directory
 
+# Defining a class named UserViewSet that inherits from viewsets.ModelViewSet
 class UserViewSet(viewsets.ModelViewSet):
+    # Setting the queryset attribute to retrieve all User objects from the database
     queryset = User.objects.all()
+    # Setting the serializer_class attribute to use the UserSerializer for serialization and deserialization
     serializer_class = UserSerializer
