5714148037b83a73ab5535df91782e8888cf1018
ellenfel <ellfelfen@gmail.com>
2024-09-10 09:56:03 +0300

    Created serializers for users and tasks

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Task model.
+
+    Attributes:
+        model (Task): The Task model class.
+        fields (list): A list of all fields to be serialized.
+    """
     class Meta:
         model = Task
         fields = '__all__'
--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,16 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for the User model.
+
+    This serializer is used to convert User model instances into JSON representation and vice versa.
+
+    Attributes:
+        model (User): The User model class.
+        fields (list): A list of fields to include in the serialized representation. If set to '__all__', all fields will be included.
+
+    """
     class Meta:
         model = User
         fields = '__all__'
