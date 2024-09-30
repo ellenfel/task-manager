ce49fd6c830c64287dd498e35ec746157618c035
ellenfel <ellfelfen@gmail.com>
2024-09-30 17:51:18 +0300

    handled fetching and displaying data with TaskPage.js

--- frontend/src/pages/TaskPage.js
+++ frontend/src/pages/TaskPage.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getTasks } from '../services/api';
 
 const TaskPage = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const tasks = await getTasks();
+      setTasks(tasks);
+    };
+
+    fetchTasks();
+  }, []);
+
   return (
     <div>
       <h1>Tasks</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
